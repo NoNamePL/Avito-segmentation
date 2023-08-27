47e1bce76b26787995d1f75ec04bc9c37521a407
Roman <rdymshakov@inbox.ru>
2023-08-27 17:15:50 +0300

    add .env

--- db/db.go
+++ db/db.go
@@ -3,28 +3,35 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 )
 
 type Segment struct {
-	userID  int      `json:"user_id"`
-	Items []string `json:"Items"`
+	userID int      `json:"user_id"`
+	Items  []string `json:"Items"`
 }
 
 type Segmentations struct {
-	Segments	[]Segment
+	Segments []Segment
 }
+
 const (
-	host     = "localhost"
-	port     = 5432
-	user     = "admin"
-	password = "76705315Rr"
-	dbname   = "Avito_segmentations"
+	host   = "localhost"
+	port   = 5432
+	user   = "admin"
+	dbname = "Avito_segmentations"
 )
 
 var db *sql.DB
 
 func Init() (*sql.DB, error) {
-	psqlInfo := fmt.Sprintf("postgres://%s:%s@localhost:%d/%s?sslmode=disable", user, password, port, dbname)
+
+	psqlPassword, ok := os.LookupEnv("PSQLPass")
+	if !ok {
+		log.Fatal("Can't connect to .env")
+	}
+	psqlInfo := fmt.Sprintf("postgres://%s:%s@localhost:%d/%s?sslmode=disable", user, psqlPassword, port, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, err
